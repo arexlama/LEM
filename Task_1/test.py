f2daa12c5244294e44581cabd4448b73b249c6f0
arexlama <161947351+arexlama@users.noreply.github.com>
2024-03-10 21:10:02 +0100

    Update test.py

--- Task_1/test.py
+++ Task_1/test.py
@@ -3,8 +3,8 @@ komunikat = ''
 
 def test():
     global komunikat
-    if code_1.radio.id == 42:
-        komunikat = 'ID radia jest poprawnie ustawione'
+    if code.radio.id == 42:
+        komunikat = 'Radio ID is set right'
         return True
-    komunikat = 'ID radia jest niepoprawnie ustawione. Pomyśl jeszcze raz.'
+    komunikat = 'Radio ID is incorrect. Think about it again.'
     return False
