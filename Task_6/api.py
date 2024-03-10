a70da5efa54f97db847700e297eefc3afc2fe169
PhanT0MMaster <107053856+PhanT0MMaster@users.noreply.github.com>
2024-03-10 19:04:15 +0100

    Przetłumaczenie api.py

--- Task_6/api.py
+++ Task_6/api.py
@@ -1,19 +1,24 @@
 def forward(n=1) -> None:
     # Don't turn and walk straight to the next crossroad (n for repeated action)
+    # Nie skręcaj i idź prosto do następnego skrzyżowania (n dla powtarzanej akcji)
     pass
 
 def right(n=1) -> None:
     # Turn right and walk straight to the next crossroad (n for repeated action)
+    # Skręć w prawo i idź prosto do następnego skrzyżowania (n dla powtarzanej akcji)
     pass
 
 def left(n=1) -> None:
     # Turn left and walk straight to the next crossroad (n for repeated action)
+    # Skręć w lewo i idź prosto do następnego skrzyżowania (n dla powtarzanej akcji)
     pass
 
 def walk_in() -> None:
     # Walk in the nearest building
+    # Wejdź do najbliższego budynku
     pass
 
 def walk_out() -> None:
     # Walk out of the building you are currently in
+    # Wyjdź z budynku, w którym aktualnie jesteś
     pass
